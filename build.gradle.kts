023f8f084393c1d874c2cbd39c6d0f3036150623
ya0igoddess <matratskingalya@gmail.com>
2024-11-28 03:58:29 +0300

    add jib

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,19 @@ plugins {
     application
     kotlin("jvm") version(libs.versions.kotlin)
     id("dev.kordex.gradle.kordex") version(libs.versions.kordex)
+    id("com.google.cloud.tools.jib") version(libs.versions.jib)
+}
+
+jib {
+    from {
+        image = "openjdk:17-alpine"
+    }
+    to {
+        image = "ziz"
+    }
+    container {
+        mainClass = "com.github.ya0igoddess.ziz.MainKt"
+    }
 }
 
 application {
